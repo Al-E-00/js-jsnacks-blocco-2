fdc1d3fd06dfb9419175df0655e50e2ecd61abdb
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-24 16:04:10 +0200

    snack 3 done -> corrected

--- snack3/js/script_snack_third.js
+++ snack3/js/script_snack_third.js
@@ -9,8 +9,8 @@ let oddNumbersSum = [];
 let sum = 0;
 
 
-    for (let i = 0; i <= numbers.length; i++) {
-        if (i % 3 === 0) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (i % 2 !== 0) {
         sum = sum + numbers[i];
         oddNumbersSum.push(numbers[i]);
     } else {
