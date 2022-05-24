991ab62dd9884f0ab100afab227a19e9293faae7
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-24 15:47:25 +0200

    snack 3 !!! problem odd numbers

---
+++ snack3/js/script_snack_third.js
@@ -0,0 +1,22 @@
+/* 
+    - make an array with whole numbers
+    - sum elements with odd index
+*/
+
+let numbers = [32, 43, 54, 122, 132, 21, 65, 43, 2, 54, 5, 65, 6];
+let oddNumbersSum = [];
+
+let sum = 0;
+
+
+    for (let i = 0; i <= numbers.length; i++) {
+        if (i % 3 === 0) {
+        sum = sum + numbers[i];
+        oddNumbersSum.push(numbers[i]);
+    } else {
+        sum = sum
+    }
+    }
+
+console.log(oddNumbersSum);
+console.log(sum);
